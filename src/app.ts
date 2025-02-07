ac84cef2ec03b369cfe13040d255a34e2efc1034
final898y <final898y@gmail.com>
2025-02-07 19:52:20 +0800

    feat. 增加cors、axios、express.static。 refacotor:調整成ES Module (#2)
    * feat. 增加cors、axios、增加express.static
    
    * feat. 調整為esModule
    
    * fix. 調整json檔導入、package.json scripts
    
    * fix. corsOptions增加前端網站

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,20 @@ import express from 'express';
 import fs from 'fs';
 import https from 'https';
 import http from 'http';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from "url";
 
 import swaggerUi from 'swagger-ui-express';
-import swaggerDocument from './swagger.json';
+import swaggerDocument from './swagger.json' with { type: 'json' };
 
-import env from './env';
-import IndexRouter from './routers/indexRouter';
-import UserRouter from './routers/userRouter';
-import TestRouter from './routers/testRouter';
+import env from './env.js';
+import IndexRouter from './routers/indexRouter.js';
+import UserRouter from './routers/userRouter.js';
+import TestRouter from './routers/testRouter.js';
 
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const app = express();
 const httpport: number = env.HTTPPORT;
 const httpsport: number = env.HTTPSPORT;
@@ -18,6 +23,18 @@ const httpsport: number = env.HTTPSPORT;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+const corsOptions = {
+  origin: [
+    'http://localhost:3000',
+    'https://localhost:443',
+    'http://localhost:5173',
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+credentials: true,
+};
+app.use(cors(corsOptions));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
@@ -25,6 +42,10 @@ app.use('/', IndexRouter);
 app.use('/users', UserRouter);
 app.use('/tests', TestRouter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/axios', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'axiosTest.html'));
+});
 
 const privateKey = fs.readFileSync('src/key.pem');
 const certificate = fs.readFileSync('src/cert.crt');
