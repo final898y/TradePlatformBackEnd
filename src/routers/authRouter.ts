18261eccc91847b148134a875acacbfb3c0e9eeb
final898y <final898y@gmail.com>
2025-05-07 20:46:23 +0800

    fix. 補充refresh Router

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import wrapAsync from '../helpers/wrapAsyncHelper.js';
 import * as googleAuthController from '../controllers/googleAuthController.js';
+import * as AuthController from '../controllers/AuthController.js';
 
 const router = express.Router();
 
 router.post('/verifyGoogleIdToken', wrapAsync(googleAuthController.verifyGoogleIdToken));
 router.get('/getCsrfToken', wrapAsync(googleAuthController.getCsrfToken));
-
+router.get('/refresh', wrapAsync(AuthController.refresh));
 export default router;
